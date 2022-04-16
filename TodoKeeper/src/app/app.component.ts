5bb04fade6287269bb630cdebeb988bed9c59cd1
Hiago Koziel <hiagokoziel100@gmail.com>
2022-04-16 21:09:28 +0200

    Adding validation to check if task list is empty

--- TodoKeeper/src/app/app.component.ts
+++ TodoKeeper/src/app/app.component.ts
@@ -8,17 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'TodoKeeper';
 
-  public tasks : string[] = [];
+  public tasks: string[] = [];
   public newTask = "";
+  containsTask = false;
 
   public addTask() {
     if (this.newTask != '') {
       this.tasks.push(this.newTask);
       this.newTask = '';
+      this.containsTask = true;
     }
   }
 
   public deleteTask(position: number) {
     this.tasks.splice(position, 1);
-}
+    if (this.tasks.length === 0) {
+      this.containsTask = false;
+    }
+  }
 }
